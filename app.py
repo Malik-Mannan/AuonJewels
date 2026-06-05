12222a128f552e02cb4632fee2c22fd8258fb197
Malik Mannan <malikmannan1133@gmail.com>
2026-06-05 18:00:19 +0500

    switch to brevo smtp

--- app.py
+++ app.py
@@ -24,13 +24,10 @@ ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD')
 # ══════════════════════════════════════════════
 #  EMAIL CONFIG
 # ══════════════════════════════════════════════
-app.config['MAIL_SERVER']   = 'smtp.gmail.com'
-app.config['MAIL_PORT']     = 465
-app.config['MAIL_USE_TLS']  = False
-app.config['MAIL_USE_SSL']  = True
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
-app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_SERVER']  = os.environ.get('MAIL_SERVER')
+app.config['MAIL_PORT']    = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
 mail = Mail(app)
 
 # ══════════════════════════════════════════════
